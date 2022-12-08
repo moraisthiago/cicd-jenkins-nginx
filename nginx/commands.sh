abef1d610ecd126baa062b7ff83f4241a1370a10
moraisthiago <thiagueson@gmail.com>
2022-12-08 16:45:08 -0300

    docker-compose jenkins e nginx

---
+++ nginx/commands.sh
@@ -0,0 +1,12 @@
+docker-compose up -d
+docker-compose ps
+docker-compose exec [node] apk add bash nano
+docker-compose exec [node] bash
+
+nano /etc/nginx/nginx.conf
+nano /etc/nginx/conf.d/default.conf
+nano /usr/share/nginx/html/index.html
+nginx -t
+nginx -s reload
+
+tail -f /var/log/nginx/nginx-access.log
